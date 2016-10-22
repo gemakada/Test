14410e9fa5719dfc8bdccfa030b8cf22301c87fb
makan <makan@DESKTOP-EH4DT3E>
2016-10-22 20:00:36 +0300

    Laptop Commited

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -12,6 +12,8 @@ namespace TestGit
         {
             Console.WriteLine("Hello1");
             Console.WriteLine("Change1");
+            Console.WriteLine("Change from Laptop");
+            Console.WriteLine("Change again from Laptop");
         }
     }
 }
